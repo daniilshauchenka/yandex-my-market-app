3c796a9efa550d854132b094a59cfaaa80dfc9fd
Шевченко Даниил Александрович <daniil.shauchenka@softclub.by>
2026-05-08 17:52:09 +0300

    paging

--- src/main/java/ru/yandex/practicum/mymarket/controller/CartController.java
+++ src/main/java/ru/yandex/practicum/mymarket/controller/CartController.java
@@ -1,5 +1,35 @@
 package ru.yandex.practicum.mymarket.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.yandex.practicum.mymarket.enums.Action;
+import ru.yandex.practicum.mymarket.service.CartService;
+
+@Controller
+@RequestMapping("/cart/items")
+@RequiredArgsConstructor
 public class CartController {
 
+    private final CartService cartService;
+
+    @GetMapping
+    public String cart(Model model) {
+        model.addAttribute("items", cartService.getCartItems());
+        model.addAttribute("total", cartService.getTotal());
+        return "cart";
+    }
+
+    @PostMapping
+    public String changeCartItem(
+        @RequestParam Long id,
+        @RequestParam Action action
+    ) {
+        cartService.changeCount(id, action);
+        return "redirect:/cart/items";
+    }
 }
