78b96ef9b66d68b246e9c1e1afd0b81722d24a12
Шевченко Даниил Александрович <daniil.shauchenka@softclub.by>
2026-05-08 17:03:03 +0300

    repositories

---
+++ src/main/java/ru/yandex/practicum/mymarket/repository/CartItemRepository.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.mymarket.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.yandex.practicum.mymarket.entity.CartItem;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    Optional<CartItem> findByItemId(Long itemId);
+
+    List<CartItem> findAllByOrderByIdAsc();
+}
